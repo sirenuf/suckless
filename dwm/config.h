c49ce81f5e410e7a56ff099c36cedf0f3a8bef3b
sirenuf <10t00lbox0@disroot.org>
2024-04-06 16:55:36 +0200

    change exit command binding

--- dwm/config.h
+++ dwm/config.h
@@ -117,7 +117,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_Return,      quit,           {0} },
 
         { 0,            XF86XK_AudioLowerVolume,        spawn,  {.v = downvol } },
         { 0,            XF86XK_AudioMute,               spawn,  {.v = mutevol } },
