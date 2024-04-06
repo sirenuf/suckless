b84dc5c2a8bd38d1898ca636e07539fbe3aba033
sirenuf <10t00lbox0@disroot.org>
2024-04-06 17:22:09 +0200

    fix config

--- slock/config.h
+++ slock/config.h
@@ -1,6 +1,6 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *user  = "viktor";
+static const char *group = "viktor";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
