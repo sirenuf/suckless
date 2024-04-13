25c0912cd4eedbd01e6529516ab581a6c998cf0c
sirenuf <10t00lbox0@disroot.org>
2024-04-13 22:31:26 +0200

    add fadeinactive patch and tweak settings

--- dwm/config.h
+++ dwm/config.h
@@ -33,6 +33,9 @@ static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const double activeopacity   = 1.0f;     /* Window opacity when it's focused (0 <= opacity <= 1) */
+static const double inactiveopacity = 0.6f;   /* Window opacity when it's inactive (0 <= opacity <= 1) */
+static Bool bUseOpacity             = True;     /* Starts with opacity on any unfocused windows */
 static const char *fonts[]          = { "Terminess Nerd Font:size=11" };
 static const char dmenufont[]       = "Terminess Nerd Font:size=11";
 static const char col_gray1[]       = "#222222";
@@ -101,6 +104,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ MODKEY,                       XK_a,      toggleopacity,  {0} },
 	{ MODKEY,             		XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
