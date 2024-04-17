9084b741c6e01a424a8fe113cf54df398e5195c8
sirenuf <10t00lbox0@disroot.org>
2024-04-17 20:34:30 +0200

    adjust transparency on inactive and deleted config.def.h

--- dwm/config.h
+++ dwm/config.h
@@ -34,7 +34,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const double activeopacity   = 1.0f;     /* Window opacity when it's focused (0 <= opacity <= 1) */
-static const double inactiveopacity = 0.6f;   /* Window opacity when it's inactive (0 <= opacity <= 1) */
+static const double inactiveopacity = 0.95f;   /* Window opacity when it's inactive (0 <= opacity <= 1) */
 static Bool bUseOpacity             = True;     /* Starts with opacity on any unfocused windows */
 static const char *fonts[]          = { "Terminess Nerd Font:size=11" };
 static const char dmenufont[]       = "Terminess Nerd Font:size=11";
